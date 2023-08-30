bb77de5f7336d1af530264006e640a049b887e9b
chigozieokoroafor <okoroaforc14@gmail.com>
2023-08-30 16:47:02 +0100

    added blueprint for shifts

--- folder/routes/providers/__init__.py
+++ folder/routes/providers/__init__.py
@@ -1,7 +1,9 @@
 from flask import Blueprint
 # from folder.routes.auth import auth
 from folder.routes.providers.others import others
+from folder.routes.providers.shifts import prov_shifts
 
 provider = Blueprint("provider", __name__)
 
 provider.register_blueprint(others)
+provider.register_blueprint(prov_shifts)
